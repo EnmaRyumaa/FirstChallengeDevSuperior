f4481ba58d3c8a43f6a910e801f3197fca9250e2
EnmaRyumaa <fer.augustofs@gmail.com>
2024-11-27 12:08:29 -0300

    Services created

--- src/main/java/com/firstchallengedevsuperior/firstchallenge/entities/Order.java
+++ src/main/java/com/firstchallengedevsuperior/firstchallenge/entities/Order.java
@@ -10,7 +10,12 @@ public class Order {
     public Order(Integer code, double basic, double discount) {
         this.code = code;
         this.basic = basic;
-        this.discount = discount;
+
+        if(discount == 0.00) {
+            this.discount = 1.00;
+        } else {
+            this.discount = discount / 100;
+        }
     }
 
     public Integer getCode() {
