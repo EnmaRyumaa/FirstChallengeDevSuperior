d8b1daad5aadd2eedb1bf6222ba3536004915d1a
EnmaRyumaa <fer.augustofs@gmail.com>
2024-11-27 11:09:10 -0300

    Project created

---
+++ src/main/java/com/firstchallengedevsuperior/firstchallenge/FirstchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.firstchallengedevsuperior.firstchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstchallengeApplication.class, args);
+	}
+
+}
